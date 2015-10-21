dfa2b77f7c128788dda861f5f1cf3e22a345c4b7
Anton Medvedev <anton.medvedev@mandosys.com>
2015-10-21 15:08:43 +0300

    #11: Some graph tests added

--- graph.js
+++ graph.js
@@ -6,49 +6,47 @@ module.exports = function (vertexCount, name) {
     var edges = [];
     var circles = [[]];
 
-    var initEdges = function(edges) {
-		for (var i = 0; i < vertexCount; i++) {
-			edges[i] = [];
-			for (var j = 0; j < vertexCount; j++) {
-				edges[i][j] = false;
-			}
+	for (var i = 0; i < vertexCount; i++) {
+		edges[i] = [];
+		for (var j = 0; j < vertexCount; j++) {
+			edges[i][j] = false;
 		}
-    }
-
-	initEdges(edges);
+	}
 
 	for (var i = 0; i < vertexCount; i++) {
 		circles[0].push(i);
 	}
 
-	this.getVertex = function(i) { return vertices[i]; };
+	this.getVertex = function(i) {
+
+        if (i >= vertexCount)
+            throw new Error('Vertex index must me less than ' + vertexCount);
+
+		return vertices[i];
+	};
 
 	this.setVertex = function(i, value) {
-		vertices[i] = value;
 
+        if (i >= vertexCount)
+            throw new Error('Vertex index must me less than ' + vertexCount);
+
+		vertices[i] = value;
 		return this;
 	};
 
 	this.getVertexCount = function () { return vertexCount; }
 
-	this.getEdge = function (i, j) {
-
-        if (i >= vertexCount || j >= vertexCount)
-            throw 'Incorrect i or j';
-
-		return edges[i][j];
-	}
+	this.getEdge = function (i, j) { return edges[i][j]; }
 
 	this.setEdge = function (i, j, value) {
-        
-        if (i >= vertexCount || j >= vertexCount)
-            throw 'Not good';
-        
-        if (i == j)
-            throw 'No loops';
 
-		edges[i][j] = !edges[i][j];
-		edges[j][i] = !edges[j][i];
+        if (i == j)
+            throw new Error('A graph cannot contain loops');
+			
+		if (value === undefined)
+			value = true;
+
+		edges[i][j] = edges[j][i] = value;
 
 		return this;
 	}
@@ -81,7 +79,6 @@ module.exports = function (vertexCount, name) {
 
 		var margin = 5;
 		var circleIndex = this.getVertexCircle(i);
-		//console.log(circleIndex);
 		var vertexAngle = 2 * Math.PI * (i / circles[circleIndex].length);
 		var center = { x: 50 * scale.x, y: 50 * scale.y };
 		var radiusX = (50 - margin) * scale.x;
@@ -103,15 +100,6 @@ module.exports = function (vertexCount, name) {
 		}
 
 		return neighbours;
-	};
-
-	var count = function(array) {
-		var result = 0;
-
-		for (var i = 0; i < array.length; i++)
-			result += array[i].length;
-
-		return result;
 	};
 
 	var arrDiff = function(a1, a2) {
--- test.js
+++ test.js
@@ -6,9 +6,21 @@ var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 
+/*
 var testResultDir = 'test-results';
 if (!fs.existsSync(testResultDir))
 	fs.mkdirSync(testResultDir);
+*/
+	
+// getVertex
+assert.strictEqual(undefined, new Graph(3).getVertex(2));
+
+assert.throws(function () { new Graph(3).getVertex(3); }, function (error) { return error.message === 'Vertex index must me less than 3'; });
+
+// setVertex
+assert.deepEqual({x:1, y:2}, new Graph(3).setVertex(1, {x:1, y:2}).getVertex(1));
+
+assert.throws(function () { new Graph(2).setVertex(3); }, function (error) { return error.message === 'Vertex index must me less than 2'; });
 
 // getVertexCount
 assert.equal(3, new Graph(3).getVertexCount());
@@ -16,14 +28,14 @@ assert.equal(3, new Graph(3).getVertexCount());
 // getEdge
 assert.equal(false, new Graph(3).getEdge(0, 2));
 
-assert.throws(function () { new Graph(3).getEdge(3, 1); }, function (error) { return error == 'Incorrect i or j'; });
+assert.throws(function () { new Graph(3).getEdge(3, 1); }, function (error) { return true; });
 
 // setEdge
 assert.equal(true, new Graph(3).setEdge(1, 2).getEdge(1, 2));
 
-assert.throws(function () { new Graph(3).setEdge(1, 1); }, function (error) { return error == 'No loops'; });
+assert.throws(function () { new Graph(3).setEdge(1, 1); }, function (error) { return error.message === 'A graph cannot contain loops'; });
 
-assert.throws(function () { new Graph(3).setEdge(1, 4); }, function (error) { return error == 'Not good'; });
+assert.throws(function () { new Graph(3).setEdge(1, 4); }, function (error) { return true; });
 
 // forEachVertex
 var num = 0;
@@ -31,16 +43,24 @@ var action = function () { num++; };
 new Graph(3).forEachVertex(action);
 assert.equal(3, num);
 
+var verteces = [];
+var action = function (i) { verteces.push(i); };
+new Graph(4).forEachVertex(action);
+assert.deepEqual([0, 1, 2, 3], verteces);
+
 // forEachEdge
 var num = 0;
 var action = function () { num++; };
 new Graph(4).setEdge(0, 1).setEdge(1, 2).setEdge(3, 1).setEdge(2, 3).forEachEdge(action);
 assert.equal(4, num);
 
-// toSvg
-fs.writeFile(path.join(testResultDir, 'graph.svg'), toSvg(new Graph(4).setEdge(0, 1).setEdge(1, 2).setEdge(3, 1).setEdge(2, 3).setEdge(0, 3)));
+var edges = [];
+var action = function (i, j) { edges.push([i, j]); };
+new Graph(4).setEdge(0, 1).setEdge(3, 1).forEachEdge(action);
+assert.deepEqual([[0, 1], [1, 3]], edges);
 
 // getVertex
+/*
 var testDirName = 'test-data'
 fs.readFile(path.join(testDirName, 'k2.xml'), 'utf8', function (err, xml) {
 	if (err) throw err;
@@ -52,6 +72,12 @@ fs.readFile(path.join(testDirName, 'k2.xml'), 'utf8', function (err, xml) {
 		assert.deepEqual({ x:90, y:90 }, graph.getVertex(1));
 	});
 });
+*/
+
+// getVertexCoords
+assert.deepEqual(
+	{x:1,y:2},
+	new Graph(6).setVertex(1, {x:1, y:1}).getVertexCoords(1, {x:1, y:2}));
 
 // getNeighbours
 assert.deepEqual(
@@ -67,6 +93,16 @@ assert.deepEqual(
 	[[0], [1, 2], [3, 4], [5]],
 	new Graph(6).setEdge(0, 1).setEdge(0, 2).setEdge(1, 3).setEdge(3, 2).setEdge(4, 2).setEdge(4, 5).getLevels(0));
 
+// getVertexCircle
+assert.equal(1, new Graph(2).setVertexCircle(1, 1).getVertexCircle(1));
+
+// setVertexCircle
+// todo:
+
+/*
+// toSvg
+fs.writeFile(path.join(testResultDir, 'graph.svg'), toSvg(new Graph(4).setEdge(0, 1).setEdge(1, 2).setEdge(3, 1).setEdge(2, 3).setEdge(0, 3)));
+
 // toLeveledSvg
 fs.writeFile(path.join(testResultDir, 'levs0.svg'), toLeveledSvg(new Graph(6, 'levs').setEdge(0, 1).setEdge(0, 2).setEdge(1, 3).setEdge(3, 2).setEdge(4, 2).setEdge(4, 5), 0));
 
@@ -81,10 +117,9 @@ fs.readFile(path.join(testDirName, 'k3,3.xml'), 'utf8', function (err, xml) {
 		fs.writeFile(path.join(testResultDir, 'k3,3_levs.svg'), toLeveledSvg(graph, 0));
 	});
 });
+*/
 
-// getVertexCircle
-assert.equal(1, new Graph(2).setVertexCircle(1, 1).getVertexCircle(1));
-
+/*
 fs.readFile(path.join(testDirName, 'petersen.xml'), 'utf8', function (err, xml) {
 	if (err) throw err;
 	
@@ -95,3 +130,4 @@ fs.readFile(path.join(testDirName, 'petersen.xml'), 'utf8', function (err, xml) 
 		assert.equal(0, graph.getVertexCircle(5));
 	});
 });
+*/
